531fef07ba71d717fecde4bf8cff22abf39d20de
highlander santos <highlander@lanlink.com.br>
2022-08-17 16:31:59 -0300

    :boom: fix: Revertendo react dev-tools

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MyContextProvider } from '../hooks/useContext';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -10,7 +9,6 @@ function MyApp({ Component, pageProps }) {
       <MyContextProvider>
         <Component {...pageProps} />
       </MyContextProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
